8ff25e1334d495b273230e09c8383dfe902e61e0
Diego Andres Fonseca Soto <dandresfs_04@hotmail.com>
2015-10-22 16:16:22 -0500

    Reporte por corte

--- financiero/urls.py
+++ financiero/urls.py
@@ -5,9 +5,9 @@ from financiero import views
 urlpatterns = [
     url(r'^$', login_required(views.FinancieroView.as_view())),
 
-    url(r'^gestores/corte/(?P<corte_id>\w+)/gestor/(?P<gestor_id>\w+)/$', login_required(views.GestorCorteEvidenciaView.as_view())),
-    url(r'^gestores/corte/(?P<corte_id>\w+)/gestor/(?P<gestor_id>\w+)/reporte/$', login_required(views.reporte_gestor)),
-    url(r'^gestores/corte/(?P<corte_id>\w+)/gestor/(?P<gestor_id>\w+)/email/$', login_required(views.reporte_gestor_email)),
+    url(r'^gestores/(?P<tipo_id>\w+)/corte/(?P<corte_id>\w+)/gestor/(?P<gestor_id>\w+)/$', login_required(views.GestorCorteEvidenciaView.as_view())),
+    url(r'^gestores/(?P<tipo_id>\w+)/corte/(?P<corte_id>\w+)/gestor/(?P<gestor_id>\w+)/reporte/$', login_required(views.reporte_gestor)),
+    url(r'^gestores/(?P<tipo_id>\w+)/corte/(?P<corte_id>\w+)/gestor/(?P<gestor_id>\w+)/email/$', login_required(views.reporte_gestor_email)),
 
     url(r'^gestores/$', login_required(views.GestorView.as_view())),
     url(r'^gestores/(?P<tipo_id>\w+)/$', login_required(views.GestorTipoView.as_view())),
