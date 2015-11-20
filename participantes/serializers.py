4be07d05ffcb87d35cece65fe605a35367df8cbe
Diego Andres Fonseca Soto <dandresfs_04@hotmail.com>
2015-11-20 12:06:01 -0500

    Departamento y Municipio grupo

--- participantes/serializers.py
+++ participantes/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
-from formacion.models import ParticipanteEscuelaTic
+from formacion.models import ParticipanteEscuelaTic, Grupo
+from municipio.models import Municipio
+from departamento.models import Departamento
+
+class DepartamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Departamento
+        fields = ('nombre',)
+
+class MunicipioSerializer(serializers.ModelSerializer):
+    departamento = DepartamentoSerializer()
+    class Meta:
+        model = Municipio
+        fields = ('nombre','departamento')
+
+class GrupoSerializer(serializers.ModelSerializer):
+    municipio = MunicipioSerializer()
+    class Meta:
+        model = Grupo
+        fields = ('municipio',)
 
 class ParticipanteSerializer(serializers.ModelSerializer):
+    grupo = GrupoSerializer()
     class Meta:
         model = ParticipanteEscuelaTic
-        fields = ('cedula','nombres','apellidos','departamento','municipio')
+        fields = ('cedula','nombres','apellidos','grupo')
