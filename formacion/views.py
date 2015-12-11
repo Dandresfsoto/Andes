c61a9cabb070d80e89f54fec77da90d76c62083d
Diego Andres Fonseca Soto <dandresfs_04@hotmail.com>
2015-12-11 12:39:38 -0500

    Orden soportes Escuela Tic

--- formacion/views.py
+++ formacion/views.py
@@ -624,7 +624,7 @@ class CalificarGrupoView(FormacionMixin,TemplateView):
 
     def get_context_data(self, **kwargs):
         participantes = ParticipanteEscuelaTic.objects.filter(grupo__id=self.kwargs['grupo_id']).count()
-        soportes = SoporteEntregableEscuelaTic.objects.filter(grupo__id=self.kwargs['grupo_id']).order_by('entregable__actividad__id')
+        soportes = SoporteEntregableEscuelaTic.objects.filter(grupo__id=self.kwargs['grupo_id']).order_by('entregable__id')
         id_actividades = soportes.values_list('entregable__actividad__id',flat=True)
         id_actividades = list(set(id_actividades))
         y=[]
