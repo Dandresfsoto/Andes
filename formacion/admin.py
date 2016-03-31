2e7203e748616f2821880ba4803965cda072fe63
Dandresfs <dandresfsoto@gmail.com>
2016-03-31 15:04:59 -0500

    Reporte proyectos ok 2

--- formacion/admin.py
+++ formacion/admin.py
@@ -1522,7 +1522,7 @@ def reporte_proyectos(modeladmin,request,queryset):
         for participante in EvidenciaDocentes.objects.filter(entregable__id=60).exclude(soporte__soporte=None):
                 row_num += 1
                 row = [
-                    participante.participante.region.nombre,
+                    participante.participante.formador.region.nombre,
                     participante.participante.formador.nombre,
                     participante.participante.nombres,
                     participante.participante.apellidos,
