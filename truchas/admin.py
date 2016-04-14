d87ba1b92ddebc2bc1b4dace973ff5ffef77c90d
Dandresfs <dandresfsoto@gmail.com>
2016-04-14 13:00:20 -0500

    Docentes masivos ok encabezado 6

--- truchas/admin.py
+++ truchas/admin.py
@@ -1153,6 +1153,7 @@ def generar_listas_docentes(modeladmin,request,queryset):
 
     for codigo_masivo in queryset:
 
+        participantes = ParticipanteDocenteMasivo.objects.filter(codigo_masivo=codigo_masivo)
 
         sesiones = [{'nivel':2,'sesion':1,'nombre_sesion':'Estructuración del Proyecto','actividades':[('Fundamentando conceptos',4)]},
                         {'nivel':2,'sesion':2,'nombre_sesion':'Cierre de Nivel','actividades':[('Valorando la secuencia didáctica',3),('Evaluando Competencias',1)]},
@@ -1179,8 +1180,9 @@ def generar_listas_docentes(modeladmin,request,queryset):
             sesion_file.Range("AC8").Value = codigo_masivo.departamento.encode("latin1")
             sesion_file.Range("AC9").Value = codigo_masivo.municipio.encode("latin1")
 
-            sesion_file.Range("AC11").Value = 'C.C. '+codigo_masivo.formador.encode("latin1")
-            sesion_file.Range("AD11").Value = 'Lugar de la Formación (IE): '.encode("latin1")+codigo_masivo.lugar.encode("latin1")
+            sesion_file.Range("AC11").Value = codigo_masivo.formador.encode("latin1")
+            sesion_file.Range("AD11").Value = 'C.C. '+codigo_masivo.cedula
+            sesion_file.Range("L12").Value = 'Lugar de la Formación (IE): '.encode("latin1")+codigo_masivo.lugar.encode("latin1")
 
             sesion_file.Range("AF6").Value = sesion['nombre_sesion'].encode("latin1")
 
@@ -1189,6 +1191,16 @@ def generar_listas_docentes(modeladmin,request,queryset):
                 sesion_file.Range("AF"+str(fila_actividad)).Value = actividad[0]
                 sesion_file.Range("AH"+str(fila_actividad)).Value = 'Horas:      '+unicode(actividad[1])
                 fila_actividad += 1
+
+            fila = 16
+            for participante in participantes:
+                sesion_file.Range("C"+str(fila)).Value = participante.nombre.encode("latin1")
+                sesion_file.Range("J"+str(fila)).Value = participante.cedula
+                sesion_file.Range("AB"+str(fila)).Value = participante.institucion
+                sesion_file.Range("AD"+str(fila)).Value = participante.sede
+                sesion_file.Range("AE"+str(fila)).Value = participante.correo
+                sesion_file.Range("AG"+str(fila)).Value = participante.telefono
+                fila += 1
 
 
     lista.Worksheets('Hoja1').Delete()
