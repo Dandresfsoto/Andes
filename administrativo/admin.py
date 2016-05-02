521ba0e4ee11c31ed9cfa87e52834d0cdc6af6b6
Dandresfs <dandresfsoto@gmail.com>
2016-05-02 18:05:11 -0500

    obligaciones

--- administrativo/admin.py
+++ administrativo/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Informes
+from .models import Informes, Obligacion
 
 admin.site.register(Informes)
+admin.site.register(Obligacion)
 
 # Register your models here.
